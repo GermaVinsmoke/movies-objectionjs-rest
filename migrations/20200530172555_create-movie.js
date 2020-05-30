c7e7be9c4e78861d733c46bd2218ea91c5e61f66
Germa Vinsmoke <vaibhav1180@gmail.com>
2020-05-30 23:02:36 +0530

    movie table

--- migrations/20200530172555_create-movie.js
+++ migrations/20200530172555_create-movie.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+	await knex.schema.createTable('movie', table => {
+		table.increments('movie_id');
+		table.string('title').notNullable();
+		table.text('overview').notNullable();
+		table.decimal('popularity').defaultTo(0);
+		table.date('release_date').notNullable();
+		table.integer('revenue').defaultTo(0);
+		table.timestamp('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+		table.timestamp('updated_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+	await knex.schema.dropTableIfExists('movie');
 };
